1a798b62cfcd1664d98350b502248f7b6c9b835a
rizkyaufaa <rizkyaufavitasarii@gmail.com>
2023-05-12 15:32:37 +0700

    document

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+class UserController extends Controller
+{
+    public function getUser(User $user)
+    {
+        $dataUser = $user->get();
+        return view('admin.user.viewUser', compact('dataUser'));
+    }
+    public function tambahForm()
+    {
+        return view('admin.user.tambahForm');
+    }
+
+    public function editForm(User $user)
+    {
+        return view('admin.user.editForm', compact('user'));
+    }
+
+    public function saveUser(User $user, Request $userRequest)
+    {
+        $data = $userRequest->all();
+@@ -28,7 +34,14 @@ public function deleteUser(User $user)
+        $user->delete();
+        return back()->with(['success' => 'Data berhasil dihapus']);
+    }
+    public function updateUser()
+    public function updateUser(User $user, Request $userRequest)
+    {
+        $data = $userRequest->all();
+
+        if ($userRequest->password) {
+            $data['password'] = bcrypt($userRequest->password);
+        }
+        $user->update($data);
+        return redirect(route('user.getUser'))->with('success', 'Data user berhasil diubah');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,28 @@
 <?php
-
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/admin', function () {
+    return view('admin/dashboard_admin');
+});
+Route::controller(UserController::class)->name('user.')->group(function () {
+    Route::get('/user', 'getUser')->name('getUser');
+    Route::get('/tambah', 'tambahForm')->name('tambahForm');
+    Route::get('/edit/{user}', 'editForm')->name('editForm');
+    Route::post('/simpan', 'saveUser')->name('saveUser');
+    Route::patch('/update', 'updateUser')->name('updateUser');
+    Route::patch('/update/{user}', 'updateUser')->name('updateUser');
+    Route::delete('/hapus/{user}', 'deleteUser')->name('deleteUser');
 });
