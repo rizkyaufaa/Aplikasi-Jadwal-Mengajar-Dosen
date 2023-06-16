7cc92c7bbf3f413714adbf11e12d473c9b6becc0
rizkyaufaa <rizkyaufavitasarii@gmail.com>
2023-06-16 14:58:32 +0700

    menambahkan middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,13 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     * 
-     * return void
+     *
+     * @return void
      */
     public function register()
     {
@@ -18,13 +19,12 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     * 
-     * return void
+     *
+     * @return void
      */
     public function boot()
     {
         config(['app.locale' => 'id']);
-        Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
     }
 }
